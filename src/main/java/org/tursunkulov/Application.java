3af6f308a786677786f2d7d9b3c0da241f0d4dbb
Khabib <110659455+saitbuilder@users.noreply.github.com>
2025-03-24 21:43:25 +0300

    Изменение директории

--- src/main/java/org/tursunkulov/Application.java
+++ src/main/java/org/tursunkulov/Application.java
@@ -1,4 +1,4 @@
-package org.example.tursunkulov;
+package org.tursunkulov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
